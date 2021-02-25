f33d4ad4a051d5e7129fcb30b51e932fad9d22ea
HawkOne <stefano.ampolo@gmail.com>
2021-02-25 22:54:16 +0100

    Add  UART_FEES

---
+++ UART_FEES/Core/Inc/TEST.h
@@ -0,0 +1,13 @@
+/*
+ * TEST.h
+ *
+ *  Created on: 20 feb 2021
+ *      Author: stefa
+ */
+
+#ifndef INC_TEST_H_
+#define INC_TEST_H_
+
+void Testiamo();
+
+#endif /* INC_TEST_H_ */
---
+++ UART_FEES/Core/Src/TEST.c
@@ -0,0 +1,30 @@
+/*
+ * TEST.c
+ *
+ *  Created on: 20 feb 2021
+ *      Author: stefa
+ */
+
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include <stdio.h>
+
+#include "TEST.h"
+
+
+
+UART_HandleTypeDef huart3;
+
+
+void Testiamo(){
+
+	char BUFFERTESTIAMO[] = "TEST ";
+	HAL_UART_Transmit(&huart3, BUFFERTESTIAMO, sizeof(BUFFERTESTIAMO)-1 , 100);
+}
